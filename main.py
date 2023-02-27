6c3e8e65b5be8fef2f0ebd5660d9e6d239173f12
Marion <mrobert148@gmail.com>
2023-02-27 10:58:54 +0100

    create ball class and move the ball

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
+import time
 
 # set up the playground
 screen = Screen()
@@ -11,7 +13,7 @@ screen.tracer(0)
 line = Turtle()
 line.hideturtle()
 line.pencolor("white")
-line.pensize(5)
+line.pensize(3)
 line.penup()
 line.goto(0, 295)
 line.setheading(270)
@@ -22,8 +24,9 @@ for n in range(15):
     line.forward(20)
 
 # create players --> player1 and player 2
-player_one = Paddle(410, 0)
-player_two = Paddle(-410, 0)
+player_one = Paddle((410, 0))
+player_two = Paddle((-410, 0))
+ball = Ball()
 screen.update()
 
 screen.listen()
@@ -34,7 +37,10 @@ screen.onkey(fun=player_two.move_down, key="q")
 
 game_on = True
 while game_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+
 
 #  the ball --> avance tout le temps
 #    - si tape un bord, renvoyer vers un autre angle maix avance toujours
--- paddle.py
+++ paddle.py
@@ -2,13 +2,13 @@ from turtle import Turtle
 
 
 class Paddle(Turtle):
-    def __init__(self, x_position, y_position):
+    def __init__(self, position):
         super(Paddle, self).__init__()
         self.shape("square")
         self.color("white")
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
-        self.setposition(x_position, y_position)
+        self.setposition(position)
 
     def move_up(self):
         if self.ycor() < 240:
